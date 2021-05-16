a28e8e50453012a3ceb0eb9da17024111369bf93
stephin007 <stephinreji123@gmail.com>
2021-05-16 21:10:10 +0530

    Added copy to clipboard feature

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
+  const hexValue = `#${hexColor}`;
   const bcg = rgb.join(",");
-  // const copyToCB = () => {
-  //     setAlert(true)
-  // }
+  const copyToCB = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  };
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeOut);
+  }, [alert]);
   return (
     <>
       <article
-        style={{ backgroundColor: `rgb(${bcg})` }}
         className={`color ${index > 10 && `color-light`}`}
+        style={{ backgroundColor: `rgb(${bcg})` }}
+        onClick={copyToCB}
       >
         <p className="percent-value">{weight}%</p>
+        <p className="color-value">{hexValue}</p>
+        {alert && <p className="alert">copied to clipboard</p>}
       </article>
     </>
   );
